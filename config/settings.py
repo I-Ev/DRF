461b1a24b7ee2b7aa48b37aa11f364fb2820d9d6
Ilya Evlanov <ilya.evlanov@gmail.com>
2023-09-21 15:33:33 +0300

    настройка celery для проекта

--- config/settings.py
+++ config/settings.py
@@ -161,3 +161,20 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 STRIPE_URL = 'https://api.stripe.com/v1/'
 STRIPE_API_KEY = os.getenv('STRIPE_API_KEY')
+
+
+
+# URL-адрес брокера сообщений
+CELERY_BROKER_URL = 'redis://localhost:6379' # Например, Redis, который по умолчанию работает на порту 6379
+
+# URL-адрес брокера результатов, также Redis
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+
+# Часовой пояс для работы Celery
+CELERY_TIMEZONE = "Australia/Tasmania"
+
+# Флаг отслеживания выполнения задач
+CELERY_TASK_TRACK_STARTED = True
+
+# Максимальное время на выполнение задачи
+CELERY_TASK_TIME_LIMIT = 30 * 60
